f02087123f085f65e87e5eb4c3cee80a04f1c7eb
AdityaSP <sp.aditya@gmail.com>
2024-07-08 21:07:58 +0530

    Variables, constant, datatype

--- first.ts
+++ first.ts
@@ -6,3 +6,19 @@
 // D:\training\jul8>
 
 console.log("Hello World!!!")
+
+
+let age: number = 10
+// Wrong assignment
+// a = "Hello"
+
+let fn: string = "John"
+let isEligible: boolean = true
+
+age = age + 20
+
+const PI: number = 3.14
+// wrong reassignment
+// PI = 3.145
+
+console.log("Age is", age, "Eligibility: ", isEligible, "PI is ", PI)
