2e3110aacc3a66941ee97875cac2660cc006a74e
AdityaSP <sp.aditya@gmail.com>
2024-07-08 21:37:43 +0530

    First function

---
+++ unitfunction.ts
@@ -0,0 +1,10 @@
+//definition or create a function
+function sayHello(){
+    console.log("Hello there!!!")
+}
+//usage or call a function
+sayHello()
+sayHello()
+sayHello()
+sayHello()
+sayHello()
