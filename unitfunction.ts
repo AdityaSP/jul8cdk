5a5a954d37cb60e16e863dd49ba4807d57db2c1e
AdityaSP <sp.aditya@gmail.com>
2024-07-08 21:58:41 +0530

    Function with a parameter

--- unitfunction.ts
+++ unitfunction.ts
@@ -8,3 +8,17 @@ sayHello()
 sayHello()
 sayHello()
 sayHello()
+
+function greet(n :string){
+    console.log("Hello", n)
+}
+
+greet("Aditya")
+greet("World");
+let firstn: string = "Mc"
+let ln: string = "Donald"
+let fulln: string = ln+","+firstn
+greet(fulln)
+
+
+
